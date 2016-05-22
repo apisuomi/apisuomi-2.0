0c62f6f5d756a545b211e27bcfa53555b90ecfc3
Jarkko Moilanen <jarkko.moilanen@ossoil.com>
2016-05-22 13:34:35 +0300

    refined and added mongo connection

--- components/harvester/ruby/harvest-xroad.rb
+++ components/harvester/ruby/harvest-xroad.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/ruby
 
-require_relative "includes/xroad-harvester.rb"
+
+require_relative "includes/xroad-harvester"
+require_relative "includes/database"
+require 'mongo'
+
+
+include Mongo
 
 #################################################################
 ## GET JSON list of X-Road services and process them
@@ -9,14 +15,16 @@ require_relative "includes/xroad-harvester.rb"
 # create new instance
 harvester = XroadHarvester.new
 # get entries as array
- xroadentries = harvester.get_xroad_service_list() 
+xroadentries = harvester.get_xroad_service_list() 
 #xroadrevisions = harvester.get_xroad_service_revision_list()
 # process each
 #puts "revision list: "
 #puts xroadrevisions
 
-xroadentries.each do |i|
-       entry = harvester.get_xroad_service_details(i)
-       puts entry
-end
+#xroadentries.each do |i|
+#       entry = harvester.get_xroad_service_details(i)
+#       puts entry
+#end
 
+Mongo::Logger.logger.level = Logger::WARN
+client = Mongo::Client.new(@client_host, @client_options)
---
+++ components/harvester/ruby/includes/database.rb
@@ -0,0 +1,6 @@
+@client_host = ['localhost:27017']
+@client_options = {
+  database: 'apisuomi',
+  user: 'apisuomi',
+  password: 'develop'
+}
